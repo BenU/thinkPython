0790be53416812c68d63802171df96cf33bcc377
Benjamin Unger <benjamin.d.unger@gmail.com>
2012-03-05 21:32:33 -0500

    finished exc14_6_2, starting 14_7

--- chp14/exc14_6_2.py
+++ chp14/exc14_6_2.py
@@ -1,4 +1,10 @@
+# I'm abandoning this exercise.  For some reason, os.popen() can't find some of the files and directories.  I\'m not sure
+# why.  Tracking down that bug in my or os.popen()\'s code is taking too long.  I've created checkSum's for many files,
+# am confident I could avoid duplicates but that bug is taking too long to track down.  I continue...
+
 import os
+
+noFileCount = 0
 
 def find_files(suffix, directory=None):
   if directory == None: directory = os.getcwd()
@@ -6,28 +12,49 @@ def find_files(suffix, directory=None):
   for root, dirs, files in os.walk(directory):
     for name in files:
       nameL = name.split('.')
-      if (len(nameL) > 1) and (nameL[1] == suffix):
+      if (len(nameL) > 1) and (nameL[-1] == suffix):
         path = os.path.join(root, name)
-        print path
+        # print path
         foundFiles.append(path)
   return foundFiles
   
 def creatCheckSum(fileName):
-  cmd = "openssl md5 < %s" % (fileName,)
-  fp = os.popen(cmd)
-  checkSum = fp.read()
-  fp.close()
-  return checkSum
+	print "In creatCheckSum!"
+	cmd = "openssl md5 < " + fileName
+	print "cmd = ", cmd
+	try:
+		fp = os.popen(cmd, 'r')
+	except:
+		return -1
+	try:
+		checkSum = fp.read()
+	except: 
+		return -2
+	try:
+		fp.close()
+	except:
+		return -3
+	return checkSum
+
         
-        
+musicDict = {}        
 home = os.environ['HOME']
 mp3s = find_files('mp3', home)
 m4as = find_files('m4a', home)
 musicFiles = mp3s + m4as
 for mf in musicFiles:
-  print mf
-  
-# send mf in musicFiles to creatCheckSum and check for duplicates
+	checkSum = creatCheckSum(mf)
+	if checkSum == "": break
+	if checkSum in musicDict.values():
+		print "Duplicate:", mf
+	else:
+		musicDict[mf] = checkSum
+
+for k,v in musicDict:
+	print k
+	print v
+	print
+
 
   
   
